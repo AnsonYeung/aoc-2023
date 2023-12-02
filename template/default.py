9ef9b5a10a3f562e00cc4a1dd2350cb32c5d119b
Anson Yeung <nanolify@gmail.com>
2023-12-02 13:55:59 +0800

    Use copy to clipboard instead

--- template/default.py
+++ template/default.py
@@ -11,33 +11,25 @@ import re
 import numpy as np
 import os
 import sys
-import requests
+from base64 import b64encode
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from init import get_session # type: ignore
-
-def submit(part: int, ans):
-    res = input(f"Submit answer {ans}? [Y/n]")
-    if 'N' in res or 'n' in res: return
-    day = int(os.path.basename(os.path.dirname(os.path.realpath(__file__))))
-    r = requests.post(f"https://adventofcode.com/2023/day/{day}/answer", data={"level": part, "answer": ans}, cookies={"session": get_session()})
-    res = r.text
-    main = re.search(r"(?s)<main>(.*)</main>", res)
-    if main:
-        print(main.group(1))
-    else:
-        print(res)
+def copy_to_clipboard(s: str):
+    # Copy to clipboard using OSC52
+    print(f"\x1b]52;c;{b64encode(s.encode()).decode()}\x07", end="")
 
 with open("input.txt", "r") as f:
     data = f.read().split('\n')[:-1]
 
 def part1():
     ans = 0
-    submit(1, ans)
+    print(f"Part 1: {ans}")
+    copy_to_clipboard(str(ans))
 
 def part2():
+    return
     ans = 0
-    submit(2, ans)
+    print(f"Part 2: {ans}")
+    copy_to_clipboard(str(ans))
 
 if __name__ == "__main__":
     part1()
